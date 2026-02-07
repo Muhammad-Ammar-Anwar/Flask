4a6c2f1e43092fcc7be7f628c6c3d0f3d3989cd0
Ammar.ai <mammaranver@gmail.com>
2026-02-07 06:56:51 +0500

    Add files via upload

---
+++ 2-Flask_SQLLite_Database/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, request
+import sqlite3
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect("products.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route("/init",methods=['GET'])
+def init_db():
+    conn = get_db_connection()
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS products(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        price REAL NOT NULL)
+    """)
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message":"Database init complete"})
+
+
+@app.route("/")
+def home():
+    return jsonify({"message":"Hello from our first flask Server"})
+
+
+
+
+@app.route("/products", methods=['GET'])
+def get_products():
+    conn = get_db_connection()
+    rows = conn.execute("SELECT * FROM products").fetchall()
+    conn.close
+    return jsonify([dict(row) for row in rows])
+
+@app.route("/products",methods=["POST"])
+def add_product():
+    data = request.get_json()
+    name = data.get('name')
+    price = data.get('price')
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+    "INSERT INTO products (name, price) VALUES (?, ?)",
+    (name, price)
+)
+
+    conn.commit()
+    new_id = cursor.lastrowid
+    conn.close()
+
+    new_product = {
+        "id":new_id,
+        "name": name,
+        "price" : price
+    }
+    return jsonify({"message": "Product added", "product" : new_product}), 201
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        init_db()
+    app.run(debug=True)
